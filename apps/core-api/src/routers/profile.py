66c5b3f8de1bbe7889a0fe6d87b72bbcee36426c
Konstantin Smage <kpsmage@mai.education>
2026-04-09 02:32:31 +0300

    add: profile router

---
+++ apps/core-api/src/routers/profile.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException
+
+from ..db.database import SessionDep
+from ..services.profile import ProfileService
+
+
+router = APIRouter(tags=["profile"], prefix="/profile")
+
+
+@router.get("/{user_id}")
+async def get_user_profile(
+        user_id: int,
+        session: SessionDep,
+):
+    profile_service = ProfileService(session)
+
+    user = await profile_service.get_profile_by_id(user_id)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user

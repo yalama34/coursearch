f1b4c27b96e0d2e1bbf160fe50635b4d5d867a68
Ярослав Прудников <yaprudnikov@mai.education>
2026-04-18 10:32:47 +0300

    fix(backend): add CORS middleware

--- apps/core-api/src/main.py
+++ apps/core-api/src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 
 from src.db.database import engine
@@ -34,6 +35,18 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="RecSys API",
     lifespan=lifespan
+)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(profile_router)

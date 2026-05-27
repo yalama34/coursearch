3e04e95249b65fad81be51341ba6a32ba0a3a6bc
Ярослав Прудников <yaprudnikov@mai.education>
2026-05-27 13:48:42 +0300

    fix(course-provider): fix pydemy usage, fix requirements.txt

--- apps/course-provider/src/providers/udemy/client.py
+++ apps/course-provider/src/providers/udemy/client.py
@@ -1,11 +1,13 @@
-from pydemy import Udemy
+from pydemy import UdemyClient as Client
+from pydemy.models import CourseFilter, Course as UdemyCourse
 
 
 class UdemyClient:
     def __init__(self, client_id: str, client_secret: str):
-        self.__client = Udemy(client_id=client_id, сlient_secret=client_secret)
+        self.__client = Client(client_id=client_id, client_secret=client_secret)
 
-    def get_courses_from_api(self, page: int = 1, page_size: int = 100) -> list[dict]:
-        response = self.__client.courses(page=page, page_size=page_size)
+    def get_courses_from_api(self, page: int = 1, page_size: int = 100) -> list[UdemyCourse]:
+        course_filter = CourseFilter(page=page, page_size=page_size)
+        response = self.__client.get_courses(filters=course_filter)
 
-        return response.get("results", [])
+        return response
